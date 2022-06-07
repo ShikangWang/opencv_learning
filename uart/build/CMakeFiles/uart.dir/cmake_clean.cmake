4e1ce319afdb926a4be6e656113e479ac2ff6bf2
wsk <1653484428@qq.com>
2022-06-07 17:14:14 +0800

    successfully used wiringpi uart

---
+++ uart/build/CMakeFiles/uart.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uart.dir/uart.cpp.o"
+  "uart"
+  "uart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
